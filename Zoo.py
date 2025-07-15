293ae2d67d8493e810a2cd7aa5f7b26187f7cc29
mahdi <mmsattarzadeh1382@gmail.com>
2025-07-15 15:54:18 +0330

    edit zoo.py to solve mergr coflict3

--- Zoo.py
+++ Zoo.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 
 class Animal(ABC):
-    def __init__(self, Unique_id, Name, Age, Weight, ):
+    def __init__(self, Unique_id, Name, Age, Weight):
         self.__unique_id = Unique_id
         self.__name = Name
         self.__age = Age
@@ -49,50 +49,3 @@ class Animal(ABC):
     def make_sound(self):
         pass
 
-
-class Elephant(Animal):
-    def __init__(self, unique_id, name, age, weight, body_length, tusk_length, species, habitat, lifespan):
-        super().__init__(unique_id, name, age, weight)
-        self.body_length = body_length
-        self.tusk_length = tusk_length
-        self.species = species
-        self.habitat = habitat
-        self.lifespan = lifespan
-    
-    def make_sound(self):
-        print('Pawooooo')
-    def eat(self):
-        print('12PM')
-    def sleep(self):
-        print('9AM')
-    def info(self):
-        print(f"the animal unique_id is: {self.unique_id}")
-        print(f"the animal name is: {self.name}")
-        print(f"the animal age is: {self.age}")
-        print(f"the animal weight is: {self.weight}")
-        print(f"the animal body_length is: {self.body_length}")
-        print(f"the animal tusk_length is: {self.tusk_length}")
-        print(f"the animal species is: {self.species}")
-        print(f"the animal habitat is: {self.habitat}")
-        print(f"the animal lifespan is: {self.lifespan}")
-
-e = Elephant('E001', 'Horton', 11, 3500, 450, 250, 'Asian', 'jungle', 43,)
-e.info()
-
-
-# test class
-# class cat(Animal):
-#     def make_sound(self):
-#         print('meowwww')
-#     def eat(self):
-#         print('meet')
-#     def sleep(self):
-#         print('9am')
-
-# c = cat('c002', 'fandogh', 2, 4)
-# print (c.unique_id)
-# print (c.name)
-# print(c.age)
-# c.age = 8
-# print(c.age)
-# print(c.weight)
