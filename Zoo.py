e97f34ac76a91a1badd178b1db28455fe321fbd4
mahdi <mmsattarzadeh1382@gmail.com>
2025-07-15 14:56:34 +0330

    ➖ remove Elephant class

--- Zoo.py
+++ Zoo.py
@@ -49,32 +49,3 @@ class Animal(ABC):
     def make_sound(self):
         pass
 
-
-class Elephant(Animal):
-    def __init__(self, unique_id, name, age, weight, body_length, tusk_length, species, habitat, lifespan):
-        super().__init__(unique_id, name, age, weight)
-        self.body_length = body_length
-        self.tusk_length = tusk_length
-        self.species = species
-        self.habitat = habitat
-        self.lifespan = lifespan
-    
-    def make_sound(self):
-        print('Pawooooo')
-    def eat(self):
-        print('12PM')
-    def sleep(self):
-        print('9AM')
-    def info(self):
-        print(f"the animal unique_id is: {self.unique_id}")
-        print(f"the animal name is: {self.name}")
-        print(f"the animal age is: {self.age}")
-        print(f"the animal weight is: {self.weight}")
-        print(f"the animal body_length is: {self.body_length}")
-        print(f"the animal tusk_length is: {self.tusk_length}")
-        print(f"the animal species is: {self.species}")
-        print(f"the animal habitat is: {self.habitat}")
-        print(f"the animal lifespan is: {self.lifespan}")
-
-e = Elephant('E001', 'Horton', 11, 3500, 450, 250, 'Asian', 'jungle', 43,)
-e.info()
