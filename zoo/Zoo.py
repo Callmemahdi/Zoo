c60556831f68cb3b8c54d7fe5893e72a853be273
mahdi <mmsattarzadeh1382@gmail.com>
2025-07-18 00:08:50 +0330

    ➕ write Zoo class's remove_animal method in Zoo.py and test it

--- zoo/Zoo.py
+++ zoo/Zoo.py
@@ -1,5 +1,15 @@
+#temp
+# from animals.Elephant import Elephant
 animal_list = []
 
 class Zoo():
     def add_animal(self, animal):
         animal_list.append(animal)
+
+    def remove_animal(self, unique_id):
+        for animal in animal_list:
+            if animal.unique_id == unique_id:
+                animal_list.remove(animal)
+
+# e = Elephant('E001', 'Horton', 11, 3500, 450, 250, 'Asian', 'jungle', 43,)
+# print (e.unique_id)
