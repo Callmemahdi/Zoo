9d9a35fc990314d99c281a5d37050416f74521a2
mahdi <mmsattarzadeh1382@gmail.com>
2025-07-18 18:40:48 +0330

    test count_by_type method of Zoo class in cli.main

--- cli/main.py
+++ cli/main.py
@@ -6,6 +6,10 @@ z = Zoo()
 
 l = Lion('l001', 'Oscar', 12, 150, 20, 80, 30, 'alpha', 110)
 z.add_animal(l)
+
+l = Lion('l002', 'Rex', 13, 140, 18, 84, 20, 'beta', 120)
+z.add_animal(l)
+
 s = Snake('S001', 'Jack', 3, 7, 'True', 200, 'striped', 'yellow', 25)
 z.add_animal(s)
 #print (animal_list)
@@ -17,3 +21,6 @@ z.add_animal(s)
 #z.serach_animal_by_name('Oscar')
 z.search_animal_by_id('S001')
 #print (animal_list[1].name)
+
+z.count_by_type('Lion')
+z.count_by_type('Snake')
