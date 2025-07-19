418fb7f94b1556d12e29909df2b1bfc53a5ee648
mahdi <mmsattarzadeh1382@gmail.com>
2025-07-19 10:09:16 +0330

    ➕ write the base of JsonStorage class on json_storage.py

---
+++ storage/json_storage.py
@@ -0,0 +1,11 @@
+from .abstract_storage import AbstractStorage
+
+
+class JsonStorage(AbstractStorage):
+    def save(self, animal):
+        pass
+    def load(self):
+        pass
+    def delete(self, unique_id):
+        pass
+    
