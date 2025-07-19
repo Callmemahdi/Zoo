2d90d4dfd017e6066ba36734cbcee964de986c93
mahdi <mmsattarzadeh1382@gmail.com>
2025-07-19 14:40:50 +0330

    ➕ write the save method in JsonStorage class on json_storage.py

--- storage/json_storage.py
+++ storage/json_storage.py
@@ -1,9 +1,24 @@
+import json
 from .abstract_storage import AbstractStorage
+from animals.Elephant import Elephant
+from animals.Lion import Lion
+from animals.Snake import Snake
 
 
 class JsonStorage(AbstractStorage):
-    def save(self, animal):
-        pass
+    def save(self):
+        if self.__class__.__name__ == "Lion":
+            j = Lion.make_dict()
+            with open("animals.json", "a") as f:
+                json.dump(j, f)
+        elif self.__class__.__name__ == "Snake" :
+            j = Snake.make_dict()
+            with open("animals.json", "a") as f:
+                json.dump(j, f)
+        elif self.__class__.__name__ == "Elephant" :
+            j = Elephant.make_dict()
+            with open("animals.json", "a") as f:
+                json.dump(j, f) 
     def load(self):
         pass
     def delete(self, unique_id):
