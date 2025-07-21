e59ed1ed179b682d00dd8330388c0a98436894dd
mahdi <mmsattarzadeh1382@gmail.com>
2025-07-21 11:53:33 +0330

    ➕ write the save method in CsvStorage class on csv_stor age.py

---
+++ storage/csv_storage.py
@@ -0,0 +1,27 @@
+import os
+import csv
+from .abstract_storage import AbstractStorage
+from animals.Lion import Lion
+from animals.Elephant import Elephant
+from animals.Snake import Snake
+
+
+
+class CsvStorage(AbstractStorage):
+    def save(self, l):
+        if l.__class__.__name__ == "Lion":
+            row = Lion.make_dict(l)
+        elif l.__class__.__name__ == "Snake":
+            row = Snake.make_dict(l)
+        elif l.__class__.__name__ == "Elephant":
+            row = Elephant.make_dict(l)
+        else:
+            raise ValueError("Invalid animal type")
+
+        file_exists = os.path.exists("animals.csv")
+
+        with open("animals.csv", "a", newline="") as f:
+            writer = csv.DictWriter(f, fieldnames=row.keys())
+            if not file_exists:
+                writer.writeheader()
+            writer.writerow(row)
